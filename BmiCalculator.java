3989f6140f68b2cadb16c72333ecfc0883427797
tahsin090 <tajisan789@gmail.com>
2026-07-28 19:48:42 +0600

    Completed math and boolean

---
+++ BmiCalculator.java
@@ -0,0 +1,12 @@
+public class BmiCalculator {
+    public static void main(String[] args) {
+        int weight = 75;
+        double height = 1.56;
+        double bmi = weight / (height * height);
+        System.out.println(bmi);
+        long roundedBmi = Math.round(bmi);
+        System.out.println(roundedBmi);
+        boolean isOverweight = roundedBmi > 25;
+        System.out.println(isOverweight);
+    }
+}
