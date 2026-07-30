be99ee94a6c86f80e3700fe1dd61875cd4a5faec
tahsin090 <tajisan789@gmail.com>
2026-07-30 22:00:28 +0600

    learning java class

--- BmiCalculator.java
+++ BmiCalculator.java
@@ -2,7 +2,8 @@ public class BmiCalculator {
     public static void main(String[] args) {
         int weight = 75;
         double height = 1.56;
-        double bmi = weight / (height * height);
+        // double bmi = weight / (height * height);
+        double bmi = weight / Calculator.multiplyNumber(height, height);
         System.out.println(bmi);
         long roundedBmi = Math.round(bmi);
         System.out.println(roundedBmi);
